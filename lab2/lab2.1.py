7b52120ca056790441ccf5a1fc33f6720adbc8a9
AniaSniadek <annsni2@st.amu.edu.pl>
2021-04-12 13:00:18 +0200

    add chart to 2.1

--- lab2/lab2.1.py
+++ lab2/lab2.1.py
@@ -1,5 +1,6 @@
 import numpy as np
 import csv
+import matplotlib.pyplot as pl
 
 from IPython.display import display, Math, Latex
 
@@ -15,10 +16,12 @@ for xi, yi in reader:
 def h(theta, x):
     return theta[0] + theta[1] * x
 
+# Funckja kosztu
 def funkcja_kosztu(h, theta, x, y):
     m = len(y)
     return 1.0 / (2 * m) * sum((h(theta, x[i]) - y[i])**2 for i in range(m))
 
+# Funkcja gradientu prostego
 def gradient_descent(h, cost_fun, theta, x, y, alpha, eps):
     current_cost = cost_fun(h, theta, x, y)
     log = [[current_cost, theta]]  # log przechowuje wartości kosztu i parametrów
@@ -60,11 +63,51 @@ display(Math(r'\large\textrm{Wynik:}\quad \theta = ' +
             (r' \quad J(\theta) = %.4f' % log_001[-1][0])
             + r' \quad \textrm{po %d iteracjach}' % len(log_001)))
 
-# Predykcja
+# Funckje do wyświetlania wykresu
+def regdots(x, y):    
+    fig = pl.figure(figsize=(16*.6, 9*.6))
+    ax = fig.add_subplot(111)
+    fig.subplots_adjust(left=0.1, right=0.9, bottom=0.1, top=0.9)
+    ax.scatter(x, y, c='r', s=50, label='Dane')
+    
+    ax.set_xlabel(u'Wielkość miejscowości [dzies. tys. mieszk.]')
+    ax.set_ylabel(u'Dochód firmy [dzies. tys. dolarów]')
+    ax.margins(.05, .05)
+    pl.ylim(min(y) - 1, max(y) + 1)
+    pl.xlim(min(x) - 1, max(x) + 1)
+    return fig
+
+def regline(fig, fun, theta, x):
+    ax = fig.axes[0]
+    x0, x1 = min(x), max(x)
+    X = [x0, x1]
+    Y = [fun(theta, x) for x in X]
+    ax.plot(X, Y, linewidth='2',
+            label=(r'$y={theta0}{op}{theta1}x$'.format(
+                theta0=theta[0],
+                theta1=(theta[1] if theta[1] >= 0 else -theta[1]),
+                op='+' if theta[1] >= 0 else '-')))
+
+def legend(fig):
+    ax = fig.axes[0]
+    handles, labels = ax.get_legend_handles_labels()
+    # try-except block is a fix for a bug in Poly3DCollection
+    try:
+        fig.legend(handles, labels, fontsize='15', loc='lower right')
+    except AttributeError:
+        pass
+
+# Wykres
+fig = regdots(x, y)
+regline(fig, h, best_theta_001, x)
+legend(fig)
+
+# Przedidywana liczba włamań dla 50, 100, 200 pożarów
 predicted_y = h(best_theta_001, 50)
-print(predicted_y)
+print('50 pożarów: ' + str(predicted_y))
 predicted_y = h(best_theta_001, 100)
-print(predicted_y)
+print('100 pożarów: ' + str(predicted_y))
 predicted_y = h(best_theta_001, 200)
-print(predicted_y)
+print('200 pożarów: ' + str(predicted_y))
+
 
